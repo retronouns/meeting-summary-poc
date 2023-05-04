2aad8e580b32cdda59405bf7b5726f01366f1ae6
retronouns <3486507+retronouns@users.noreply.github.com>
2023-05-03 21:01:14 -0600

    add longer mp3

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { config as dotenv } from 'dotenv'
 import axios from 'axios'
-import * as fs from 'fs'
+import { createReadStream, writeFile } from 'fs'
 import FormData from 'form-data'
 
 dotenv()
@@ -12,7 +12,7 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 const transcribe = async (path: string): Promise<string> => {
     const formData = new FormData()
     formData.append('model', 'whisper-1')
-    formData.append('file', fs.createReadStream(path))
+    formData.append('file', createReadStream(path))
     // formData.append('response_format', 'verbose_json')
 
     const response = await axios.post(TRANSCRIPTION_API, formData, {
@@ -32,8 +32,13 @@ const summarize = async (input: string): Promise<any> => {
             model: 'gpt-3.5-turbo',
             messages: [
                 {
+                    role: 'system',
+                    content: `Here is a meeting transcript: "${input}"`,
+                },
+                {
                     role: 'user',
-                    content: `Summarize the following conversation transcript as bullet points sorted by descending importance: "${input}"`,
+                    content:
+                        'Summarize the meeting transcript as bullet points sorted by descending importance.',
                 },
             ],
         },
@@ -44,22 +49,58 @@ const summarize = async (input: string): Promise<any> => {
             },
         },
     )
-    return response.data.choices
+    return response.data.choices[0].message.content
+}
+
+const actionItems = async (input: string): Promise<any> => {
+    const response = await axios.post(
+        COMPLETIONS_API,
+        {
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'system',
+                    content: `Here is a meeting transcript: "${input}"`,
+                },
+                {
+                    role: 'user',
+                    content: `Please list any action items, loose ends, or tasks discussed in the transcript that need to be completed by the participants or their coworkers.`,
+                },
+            ],
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${OPENAI_API_KEY}`,
+            },
+        },
+    )
+    return response.data.choices[0].message.content
 }
 
 const main = async () => {
     try {
-        const transcription = await transcribe('mc31.mp3')
+        const transcription = await transcribe('expert.mp3')
+        writeFile('out/transcription.txt', transcription, (err) => {
+            if (err) {
+                console.log(err)
+            }
+        })
         const summary = await summarize(transcription)
-        console.log(summary)
+        writeFile('out/summary.txt', summary, (err) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+        const items = await actionItems(transcription)
+        writeFile('out/items.txt', items, (err) => {
+            if (err) {
+                console.log(err)
+            }
+        })
     } catch (error) {
         console.error('Error in API request:', error.response.data)
     }
-    // writeFile('models.json', JSON.stringify(response.data), (err) => {
-    //     if (err) {
-    //         console.log(err)
-    //     }
-    // })
 }
 
 main()
